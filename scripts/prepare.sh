8fa52600ef0dc1c3605695894b3afd721f3b70d1
ryanxx37 <ryanxx37@gmail.com>
2026-02-19 23:06:31 -0500

    Update download link for powersOfTau28 file

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,7 +2,7 @@
 
 mkdir -p keys
 mkdir -p build
-wget -nc https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau -P ./keys
+wget -nc https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_15.ptau -P ./keys
 # circom node_modules/zk-merkle-tree/circuits/Verifier.circom --wasm --r1cs -o ./build
 cp node_modules/zk-merkle-tree/dist/Verifier.r1cs build/Verifier.r1cs
 npx snarkjs groth16 setup build/Verifier.r1cs keys/powersOfTau28_hez_final_15.ptau keys/Verifier.zkey
